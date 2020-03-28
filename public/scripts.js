8564965b7e24d8d26ad6bb9f01980f4fbb26a726
AndreNasci <andrenascimentofx@gmail.com>
2020-03-28 10:32:50 -0300

    Banco de dados da aplicação

--- public/scripts.js
+++ public/scripts.js
@@ -17,3 +17,32 @@ function onOff() {
         .classList
         .toggle("addScroll")
 }
+
+function checkFields(event) {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+    // Verifica se algum campo está vazio
+    const isEmpty = valuesToCheck.find(function(value) {
+
+        const checkIfIsString = typeof event.target[value].value == "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+
+        if(checkIfIsString && checkIfIsEmpty) {
+            return true
+        }
+    })
+
+    // Se algum campo do formulário não foi preenchido
+    if(isEmpty) {
+        event.preventDefault() //impede de enviar o formulário
+        
+        //envia um alerta para o cliente
+        alert("Por favor, preencha todos os campos.")
+    }
+}
--- server.js
+++ server.js
@@ -2,41 +2,49 @@
 const express = require ('express')
 const server = express()
 
-const ideas = [
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Cursos de Programação",
-    category: "Estudo",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
-    url: "https://github.com/"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729069.svg",
-    title: "Exercícios",
-    category: "Saúde",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
-    url: "https://github.com/"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-    title: "Meditação",
-    category: "Mentalidade",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
-    url: "https://github.com/"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729017.svg",
-    title: "Horta",
-    category: "Jardim",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
-    url: "https://github.com/"
-  },
-]
+// pega o que está sendo exportado
+const db = require("./db")
+
+
+
+// const ideas = [
+//   {
+//     img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//     title: "Cursos de Programação",
+//     category: "Estudo",
+//     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
+//     url: "https://github.com/"
+//   },
+//   {
+//     img: "https://image.flaticon.com/icons/svg/2729/2729069.svg",
+//     title: "Exercícios",
+//     category: "Saúde",
+//     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
+//     url: "https://github.com/"
+//   },
+//   {
+//     img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//     title: "Meditação",
+//     category: "Mentalidade",
+//     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
+//     url: "https://github.com/"
+//   },
+//   {
+//     img: "https://image.flaticon.com/icons/svg/2729/2729017.svg",
+//     title: "Horta",
+//     category: "Jardim",
+//     description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium numquam eum a alias",
+//     url: "https://github.com/"
+//   },
+// ]
 
 // Configuração de arquivos estáticos(css, scripts, img)
 server.use(express.static("public"))
 // vai entender que os arquivos dessa pasta estão na 
 // pasta raiz
+
+// habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 
 // Configuração do nunjucks
@@ -52,24 +60,74 @@ nunjucks.configure("views", {
 // captura o pedido do cliente para responder
 server.get("/", function(req, res) {
 
-  const reversedIdeas = [...ideas].reverse()
-  
-  let lastIdeas = []
-  // for inverso
-  for(idea of reversedIdeas) {
-    if(lastIdeas.length < 2) {
-      lastIdeas.push(idea)
+  // Consultar dados na tabela
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if(err) {
+      console.log(err)
+      return res.send("Erro no banco de dados.")
     }
-  }
-
+    const reversedIdeas = [...rows].reverse()
+    
+    let lastIdeas = []
+    // for inverso
+    for(idea of reversedIdeas) {
+      if(lastIdeas.length < 2) {
+        lastIdeas.push(idea)
+      }
+    }
   
-  return res.render("index.html", { ideas: lastIdeas })
+    
+    return res.render("index.html", { ideas: lastIdeas })
+  })
+
 })
 
 server.get("/ideias", function(req, res) {
-  const reversedIdeas = [...ideas].reverse()
 
-  return res.render("ideias.html", {ideas: reversedIdeas})
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if(err) {
+      console.log(err)
+      return res.send("Erro no banco de dados.")
+    }
+    const reversedIdeas = [...rows].reverse()
+
+    return res.render("ideias.html", {ideas: reversedIdeas})
+  })
+})
+
+server.post("/", function(req, res) {
+  // Inserir dados na tabela
+  const query = `
+    INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+    )VALUES (?,?,?,?,?); 
+  `
+  // ? -> placeholders
+  
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link,
+  ]
+
+  //cada dado do array valaues substituirá um '?'
+  db.run(query, values, function(err) {
+    if(err) {
+      console.log(err)
+      return res.send("Erro no banco de dados.")
+    }
+
+    //redireciona pra /ideiasa
+    return res.redirect("/ideias")
+  })
+
+  console.log(req.body)
 })
 
 server.listen(3000);
